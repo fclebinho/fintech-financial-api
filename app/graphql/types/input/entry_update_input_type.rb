62ebd8877c2d787b7591ee92e053ff344c46b557
Cleber Gomes <cleber.gomes@govoll.com>
2021-10-30 18:18:25 -0300

    Ajustes

--- app/graphql/types/input/entry_update_input_type.rb
+++ app/graphql/types/input/entry_update_input_type.rb
@@ -2,11 +2,11 @@ module Types
   module Input
     class EntryUpdateInputType < Types::BaseInputObject
       argument :id, String, required: true
-      argument :kind, Types::Enums::KindEnum, required: true
-      argument :description, String, required: true
-      argument :due_date, GraphQL::Types::ISO8601Date, required: true
-      argument :amount, Float, required: true
-      argument :status, Types::Enums::StatusEnum, required: true
+      argument :kind, Types::Enums::KindEnum, required: false
+      argument :description, String, required: false
+      argument :due_date, GraphQL::Types::ISO8601Date, required: false
+      argument :amount, Float, required: false
+      argument :status, Types::Enums::StatusEnum, required: false
     end
   end
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,6 @@
 class Entry < ApplicationRecord
   enum kind: [ :DEBIT , :CREDIT ]
   enum status: [ :PENDING , :CLOSED ]
+  
+  validates :description, length: { minimum: 5 }, allow_blank: false
 end
